e96d529a5944d48c6179c8d39c0f43da239c997f
ztl <hljztl@163.com>
2024-12-16 20:23:41 +0800

    update source

--- lib/page/index/index_binding.dart
+++ lib/page/index/index_binding.dart
@@ -1,6 +1,5 @@
 import 'package:fun_fluter/page/home/home_controller.dart';
 import 'package:fun_fluter/page/index/index_controller.dart';
-import 'package:fun_fluter/page/search/search_page.dart';
 import 'package:get/get.dart';
 
 class IndexBinding extends Bindings {
